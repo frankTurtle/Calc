699df816d8e898955289c3e22a335eafd3d3e5ba
Barret J. Nobel <bear.nobel@gmail.com>
2016-06-06 12:13:26 -0400

    wrote some logic for all numbers and decimal buttons being pressed

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
+    var displayValue: String? //................ string optional for the label
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -41,36 +43,124 @@ class ViewController: UIViewController {
     }
     
     @IBAction func decimalButtonPressed(sender: UIButton) {
+        if let currentvalue = displayValue{ //.............. check to see if the display value is currently nil
+            displayValue = currentvalue + "." //............ if its not then add a period to the end
+        }
+        else{
+            displayValue = "." //........................... if the user hasn't entered anything
+        }
+        
+        displayLabel.text = displayValue! //................ update the label text ( have to unwrap the value )
     }
 
     @IBAction func zeroButtonPressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "0"
+        }
+        else{
+            displayValue = "0"
+        }
+        
+        displayLabel.text = displayValue!
     }
     
     @IBAction func onePressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "1"
+        }
+        else{
+            displayValue = "1"
+        }
+        
+        displayLabel.text = displayValue!
     }
     
     @IBAction func twoPressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "2"
+        }
+        else{
+            displayValue = "2"
+        }
+        
+        displayLabel.text = displayValue!
     }
     
     @IBAction func threePressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "3"
+        }
+        else{
+            displayValue = "3"
+        }
+        
+        displayLabel.text = displayValue!
     }
     
     @IBAction func fourPressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "4"
+        }
+        else{
+            displayValue = "4"
+        }
+        
+        displayLabel.text = displayValue!
     }
     
     @IBAction func fivePressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "5"
+        }
+        else{
+            displayValue = "5"
+        }
+        
+        displayLabel.text = displayValue!
     }
     
     @IBAction func sixPressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "6"
+        }
+        else{
+            displayValue = "6"
+        }
+        
+        displayLabel.text = displayValue!
     }
     
     @IBAction func sevenPressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "7"
+        }
+        else{
+            displayValue = "7"
+        }
+        
+        displayLabel.text = displayValue!
     }
     
     @IBAction func eightPressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "8"
+        }
+        else{
+            displayValue = "8"
+        }
+        
+        displayLabel.text = displayValue!
     }
     
     @IBAction func ninePressed(sender: UIButton) {
+        if let currentValue = displayValue{
+            displayValue = currentValue + "9"
+        }
+        else{
+            displayValue = "9"
+        }
+        
+        displayLabel.text = displayValue!
     }
 }
 
