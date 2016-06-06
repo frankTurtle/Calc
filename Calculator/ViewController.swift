cb157c004ad41c5513b7126346d90a79cb9a2868
Barret J. Nobel <bear.nobel@gmail.com>
2016-06-06 12:00:52 -0400

    hooked up all buttons to IBActions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,56 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func plusMinusPressed(sender: UIButton) {
+    }
 
+    @IBAction func multiplyPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func dividePressed(sender: UIButton) {
+    }
+    
+    @IBAction func minusPressed(sender: UIButton) {
+    }
+    
+    @IBAction func plusPressed(sender: UIButton) {
+    }
+    
+    @IBAction func equalPressed(sender: UIButton) {
+    }
+    
+    @IBAction func decimalButtonPressed(sender: UIButton) {
+    }
 
+    @IBAction func zeroButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func onePressed(sender: UIButton) {
+    }
+    
+    @IBAction func twoPressed(sender: UIButton) {
+    }
+    
+    @IBAction func threePressed(sender: UIButton) {
+    }
+    
+    @IBAction func fourPressed(sender: UIButton) {
+    }
+    
+    @IBAction func fivePressed(sender: UIButton) {
+    }
+    
+    @IBAction func sixPressed(sender: UIButton) {
+    }
+    
+    @IBAction func sevenPressed(sender: UIButton) {
+    }
+    
+    @IBAction func eightPressed(sender: UIButton) {
+    }
+    
+    @IBAction func ninePressed(sender: UIButton) {
+    }
 }
 
