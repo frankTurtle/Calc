57ed0c65df84a28096f0a6f8a3ec23b27e2e5fb6
Barret J. Nobel <bear.nobel@gmail.com>
2016-06-06 12:33:32 -0400

    wrote logic for multiplication

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     
     var displayValue: String? //................ string optional for the label
+    var operand: Double? //..................... double optional for the operand
+    var operation: String? //................... string optional for the operation to be performed
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +30,41 @@ class ViewController: UIViewController {
     }
 
     @IBAction func multiplyPressed(sender: AnyObject) {
+        var valueFromDisplay: Double? //................. optional to hold the value from the display
+        
+        if let currentValue = displayValue,
+            doubleFromCurrentvalue = Double( currentValue ){ //. check both optionals and assign if appropriate
+                valueFromDisplay = doubleFromCurrentvalue
+        }
+        
+        if( valueFromDisplay == nil ){ //.................... if the value entered is invalid
+            displayLabel.text = "0" //.................... reset the label text and value to nil
+            displayValue = nil
+        }
+        else if( operand == nil ){ //..................... if operand hasnt been set yet
+            operand = valueFromDisplay //................. get the data from the label
+            operation = "*" //............................ set the operation
+            displayValue = nil //......................... clears the value b/c we already stored it
+        }
+        else if( operand != nil ){ //..................... if the operand is not nil perform the operation
+            if( operation == "+" ){
+                operand = operand! + valueFromDisplay!
+            }
+            if( operation == "-" ){
+                operand = operand! - valueFromDisplay!
+            }
+            if( operation == "*" ){
+                operand = operand! * valueFromDisplay!
+            }
+            if( operation == "/" ){
+                operand = operand! / valueFromDisplay!
+            }
+            
+            operation = "*"
+            displayValue = nil
+            displayLabel.text = operand!.description
+        }
+        
     }
     
     @IBAction func dividePressed(sender: UIButton) {
